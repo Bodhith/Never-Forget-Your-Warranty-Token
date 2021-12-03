0a055949a3e7b7a8ea58a4ff24511d50c950f10e
Bodhith <bodhith9@gmail.com>
2021-12-02 20:28:15 -0500

    second

--- APP/index.js
+++ APP/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const services = require('./services/services');
+
 const app = express();
 
 const host = "localhost";
@@ -6,8 +8,24 @@ const port = 3000;
 
 app.use(express.static('src'));
 
-app.get('/', (req, res) => {
-   res.send("jhhvb");
+app.get('/home', async (req, res) => {
+   
+   services.loadWeb3();
+   
+   var user = await services
+      .getUserType()
+      .then(function(res) {
+         console.log(1, res);
+         return res;
+      });
+   console.log(2, user);
+   if(user == "customer") {
+      res.sendFile("D:\\LEARNING\\MASTERS\\SEM 1\\BLOCK CHAIN\\PROJECT\\PHASE 4\\APP\\src\\customer\\index.html")
+   }
+   else if(user == "executive") {
+      res.sendFile("D:\\LEARNING\\MASTERS\\SEM 1\\BLOCK CHAIN\\PROJECT\\PHASE 4\\APP\\src\\executive\\index.html")
+   }
+
 });
 
 app.listen(port, host, () => {
